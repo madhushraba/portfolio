223331e2301177801c64e84258c3e8499fff96fd
Madhushraba <madhushraba21das@gmail.com>
2023-05-31 07:18:23 +0530

    darkmode

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import './navbar.css'
-// import LightModeIcon from '@mui/icons-material/LightMode';
+import React, { useEffect, useState } from "react";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import "./navbar.css";
+import LightModeIcon from "@mui/icons-material/LightMode";
 const Navbar = () => {
+  const [darkicon, setdarkicon] = useState(<DarkModeIcon />);
+  // const [first, setfirst] = useState(second)
+  // darkmode code
+  const [mode, setmode] = useState("light-mode");
+
+  useEffect(() => {
+    document.documentElement.className = mode; ///imp
+    localStorage.setItem("mode", mode);
+  }, [mode]);
+
+  const toggler = () => {
+    if (mode === "light-mode") {
+      setmode("dark-mode");
+      setdarkicon(<LightModeIcon />);
+    } else {
+      setmode("light-mode");
+      setdarkicon(<DarkModeIcon />);
+    }
+  };
   return (
     <div>
       <div className="navbar">
-        <div className="left">madhu</div>
-        <div className="right">
+        <div className="navleft">Madhushraba.dev</div>
+        <div className="navright">
           <ul className="navlist">
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
             <li>Contact</li>
-            <DarkModeIcon/>
+            {/* <DarkModeIcon/> */}
+            <button className="toggle" onClick={toggler}>
+              {darkicon}
+            </button>
             {/* <LightModeIcon/> */}
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
