e33108b98f19ef9c1740eecfc2c8817eb3609e77
Madhushraba <madhushraba21das@gmail.com>
2023-05-31 08:28:51 +0530

    home page completed

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,17 +1,43 @@
 import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 import "./home.css";
 const Home = () => {
   return (
     <div className="home">
       <div className="hometop">
-        <div className="homeleft">left</div>
-        <div className="homeright">right img</div>
+        <div className="homeleft">
+          <h1>FRONTEND REACTJS DEVELOPER</h1>
+          <p>
+            Hi i am <span> Madhushraba Das</span> i am a passionate frontend
+            developer based on Kolkata.
+          </p>
+          <div className="giticons">
+            <GitHubIcon />
+            <LinkedInIcon />
+          </div>
+        </div>
+        <div className="homeright">
+          <img
+            className="img"
+            src="https://images.pexels.com/photos/15795162/pexels-photo-15795162/free-photo-of-woman-sitting-at-the-table-full-of-candles.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+          />
+        </div>
       </div>
       <div className="techstack">
-        <div className="techl">tech</div>
+        <div className="techl">Tech Stacks:</div>
         <hr />
-        <div className="techr">icons</div>
+        <div className="techr">
+          <div className="techskill">HTML</div>
+          <div className="techskill">CSS</div>
+          <div className="techskill">Javascript</div>
+          <div className="techskill">React</div>
+          <div className="techskill">Redux</div>
+          <div className="techskill">Taillwind</div>
+          <div className="techskill">Router</div>
+        </div>
       </div>
     </div>
   );
