cb2d3ef29e50743d50b4ae081e7ca716da5c3549
Madhushraba <madhushraba21das@gmail.com>
2023-06-04 20:39:21 +0530

    'footercmplt.'

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import './footer.css'
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+import "./footer.css";
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="fooleft">Copyright © 2023. All rights are reserved</div>
+      <div className="fooright">
+        <GitHubIcon />
+        <LinkedInIcon />
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
